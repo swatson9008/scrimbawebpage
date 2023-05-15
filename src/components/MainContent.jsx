810ba9a798a6006cc7e230eefd482d77f1df0e7b
Summer <swatson9008@yahoo.com>
2023-05-15 19:16:52 -0400

    main content added

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,5 +1,19 @@
 export default function Main(){
     return (
-        <h1>Main goes here</h1>
+        <div className="mainContent"><h1>Main goes here</h1>
+        <div className="contentList">
+        <ul>
+            <li>
+                Test 1
+            </li>
+            <li>
+                Test 2
+            </li>
+            <li>
+                Test 3
+            </li>
+        </ul>
+        </div>
+        </div>
     )
 }
