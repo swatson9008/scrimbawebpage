e756161f36e4a441661ef7163bcd10ed0f9dbedf
Summer <swatson9008@yahoo.com>
2023-05-16 12:26:13 -0400

    react page done

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,15 +3,11 @@ export default function Main(){
         <div className="mainContent"><h1>Main goes here</h1>
         <div className="contentList">
         <ul>
-            <li>
-                Test 1
-            </li>
-            <li>
-                Test 2
-            </li>
-            <li>
-                Test 3
-            </li>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
         </div>
         </div>
